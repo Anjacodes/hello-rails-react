9172d54e98d4d2fb03731d5939074ed9d13b8a97
Anjacodes <anja_schmidt7@posteo.de>
2022-05-02 19:53:07 -0500

    Edit README

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # Forward all requests to GreetingsController#hello but requests must be non-AJAX (!req.xhr?) and HTML Mime type (req.format.html?).
   # DOES NOT INCLUDE ROOT PATH
- 
+
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
